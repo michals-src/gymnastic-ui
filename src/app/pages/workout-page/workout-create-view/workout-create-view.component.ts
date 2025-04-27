fd673650a0b5213d9607f8a87904d42272f1eaff
michal_src <c_ukiereczek.p@onet.pl>
2025-04-27 21:43:08 +0200

    zmiany 27.04.2025

--- src/app/pages/workout-page/workout-create-view/workout-create-view.component.ts
+++ src/app/pages/workout-page/workout-create-view/workout-create-view.component.ts
@@ -1,26 +1,115 @@
-import { Component, computed, signal, Signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, signal, Signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import {
     ControlSelectComponent,
     IControlSelectOption,
 } from '@app/shared/components/controls/control-select/control-select.component';
+import { ApiService } from '@app/shared/services/api.service';
+import { catchError, filter, finalize, Observable, of, OperatorFunction } from 'rxjs';
 import { SharedHeroIconsComponent } from '../../../shared/components/shared-hero-icons/shared-hero-icons.component';
 
-const Atlas = {
-    get: () => ({
-        default: [],
+const AtlasApi = {
+    getAtlas: () => ({
+        default: {
+            atlasMuscles: [],
+            atlasExercises: {},
+        },
         handler: 'atlas',
     }),
-    create: () => ({
+    createMuscle: () => ({
         default: null,
-        handler: '',
+        handler: 'atlas/newMuscle',
+        method: 'POST',
+    }),
+    createExercise: () => ({
+        default: null,
+        handler: 'atlas/newExercise',
+        method: 'POST',
+    }),
+    removeMuscle: (id: number) => ({
+        default: null,
+        handler: `atlas/removeMuscle/${id}`,
+        method: 'DELETE',
+    }),
+    removeExercise: (id: number) => ({
+        default: null,
+        handler: `atlas/removeExercise/${id}`,
+        method: 'DELETE',
     }),
 };
 
-const WorkoutApi = {};
+export namespace WorkoutApiModel {
+    export namespace Request {
+        export interface Create {
+            workoutId: number;
+            exerciseId: number;
+        }
+    }
+}
 
-const ApiClient = () => {};
+const ApiSchema = <
+    Fn extends (...args: any[]) => {
+        handler: string;
+        initial: any;
+        method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+        payload?: any;
+    },
+>(
+    params: Fn
+) => {
+    const a = inject(ApiService);
+    return new (class {
+        private _isLoading: WritableSignal<Boolean> = signal(false);
+        public readonly isLoading = this._isLoading.asReadonly();
+
+        private _pipe = (source$: Observable<any>): Observable<any> => {
+            return (source$.pipe as (...args: OperatorFunction<any, any>[]) => Observable<any>)();
+        };
+
+        constructor(private readonly schema: Fn) {}
+
+        pipe<T>(...args: [...OperatorFunction<T, T>[]]) {
+            this._pipe = (source$: Observable<T>): Observable<T> => {
+                return (source$.pipe as (...args: OperatorFunction<T, T>[]) => Observable<T>)(...args);
+            };
+            return this;
+        }
+
+        snapshot<O>(...args: Exclude<Parameters<Fn>, 'method'>): Observable<O> {
+            this._isLoading.set(true);
+            const params = this.schema(...args);
+            const request = (url: string) => {
+                switch (params?.method) {
+                    case 'GET':
+                        return a.get(url);
+                    case 'POST':
+                        return a.post(url, params?.payload ?? {});
+                    case 'PUT':
+                        return a.put(url, params?.payload ?? {});
+                    case 'DELETE':
+                        return a.delete(url, params?.payload ?? {});
+                    default:
+                        return a.get(url);
+                }
+            };
+            return request(params.handler).pipe(
+                catchError(() => of(params.initial)),
+                finalize(() => this._isLoading.set(false)),
+                this._pipe
+            );
+        }
+    })(params);
+};
+
+const WorkoutApi = () => {
+    return {
+        getWorkout: ApiSchema((id: number) => ({
+            handler: `workout/${id}`,
+            initial: null,
+        })),
+    };
+};
 
 @Component({
     selector: 'app-workout-create-view',
@@ -52,9 +141,9 @@ const ApiClient = () => {};
             </div>
             <button
                 (click)="clickCreateWorkout()"
-                class="px-4 py-3 rounded-xl bg-black text-white font-bold text-sm flex items-center justify-center gap-2">
+                class="px-4 py-3 rounded-xl bg-black text-white font-bold text-sm flex items-center justifyj-center gap-2">
                 <shared-hero-icons [icon]="'PlusCircleIcon'" class="size-5" />
-                @if (workoutCreateResource.isLoading()) {
+                @if (workoutApi.getWorkout.isLoading()) {
                     Pending ...
                 }
                 Utwórz
@@ -65,8 +154,9 @@ const ApiClient = () => {};
     imports: [RouterModule, FormsModule, SharedHeroIconsComponent, ControlSelectComponent],
 })
 export class WorkoutCreateViewComponent {
-    protected readonly workoutAtlasResource = ApiClient(WorkoutApi.Atlas);
-    protected readonly workoutCreateResource = ApiClient(WorkoutApi.create);
+    private readonly router = inject(Router);
+    protected readonly workoutApi = WorkoutApi();
+    // protected readonly atlasApi = AtlasApi();
 
     protected selectedMuscle: WritableSignal<IControlSelectOption> = signal(null);
     protected selectedExercise: WritableSignal<number> = signal(null);
@@ -85,10 +175,7 @@ export class WorkoutCreateViewComponent {
         }));
     });
 
-    constructor() {
-        this.atlasResource.handle();
-    }
-
+    private readonly customId: number = 32;
     protected selectMuscle(value: IControlSelectOption): void {
         this.selectedMuscle.set(value);
     }
@@ -98,9 +185,9 @@ export class WorkoutCreateViewComponent {
     }
 
     protected clickCreateWorkout(): void {
-        this.workoutCreateResource.handle({
-            muscleId: this.selectedMuscle().id,
-            exerciseId: this.selectedExercise(),
-        });
+        this.workoutApi.getWorkout
+            .snapshot(10)
+            .pipe(filter((response: any) => Boolean(response?.id)))
+            .subscribe((response) => this.router.navigate(['workout', response.id]));
     }
 }
